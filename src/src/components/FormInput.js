4243b5b4a497fb4ec9d8ddcadef1d4f1b0212585
nanthedom <naufnan@gmail.com>
2024-04-14 22:17:30 +0700

    fix: autocomplete

--- src/src/components/FormInput.js
+++ src/src/components/FormInput.js
@@ -12,6 +12,8 @@ const FormInput = ({ onSubmit }) => {
   const [submittedData, setSubmittedData] = useState(null);
   const [startSuggestions, setStartSuggestions] = useState([]);
   const [endSuggestions, setEndSuggestions] = useState([]);
+  const [startSuggestionSelected, setStartSuggestionSelected] = useState(false);
+  const [endSuggestionSelected, setEndSuggestionSelected] = useState(false);
 
   const fetchSuggestions = async (inputValue, setter) => {
     try {
@@ -27,21 +29,32 @@ const FormInput = ({ onSubmit }) => {
   };
 
   useEffect(() => {
-    fetchSuggestions(formData.startArticle, setStartSuggestions);
-  }, [formData.startArticle]);
+    if (formData.startArticle.length > 0 && !startSuggestionSelected) {
+      fetchSuggestions(formData.startArticle, setStartSuggestions);
+    } else {
+      setStartSuggestions([]);
+    }
+  }, [formData.startArticle, startSuggestionSelected]);
 
   useEffect(() => {
-    fetchSuggestions(formData.endArticle, setEndSuggestions);
-  }, [formData.endArticle]);
+    if (formData.endArticle.length > 0 && !endSuggestionSelected) {
+      fetchSuggestions(formData.endArticle, setEndSuggestions);
+    } else {
+      setEndSuggestions([]);
+    }
+  }, [formData.endArticle, endSuggestionSelected]);
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    name === 'startArticle' ? setStartSuggestionSelected(false) : setEndSuggestionSelected(false);
   };
 
-  const handleSelectSuggestion = (value, field, urlField) => {
+  const handleSelectSuggestion = (value, field, urlField, setSelected) => {
     const encodedValue = encodeURIComponent(value);
     setFormData({ ...formData, [field]: value, [urlField]: `https://en.wikipedia.org/wiki/${encodedValue}` });
     field === 'startArticle' ? setStartSuggestions([]) : setEndSuggestions([]);
+    setSelected(true);
   };
 
   const handleSubmit = (e) => {
@@ -63,14 +76,14 @@ const FormInput = ({ onSubmit }) => {
               onChange={handleChange}
               className="inline-input"
             />
-            {startSuggestions.length > 0 && (
+            {startSuggestions.length > 0 && !startSuggestionSelected && (
               <div className="suggestion-container">
                 <ul className="suggestion-list">
                   {startSuggestions.map((suggestion, index) => (
                     <li
                       key={index}
                       className="suggestion"
-                      onClick={() => handleSelectSuggestion(suggestion, 'startArticle', 'startUrl')}
+                      onClick={() => handleSelectSuggestion(suggestion, 'startArticle', 'startUrl', setStartSuggestionSelected)}
                     >
                       {suggestion}
                     </li>
@@ -90,14 +103,14 @@ const FormInput = ({ onSubmit }) => {
               onChange={handleChange}
               className="inline-input"
             />
-            {endSuggestions.length > 0 && (
+            {endSuggestions.length > 0 && !endSuggestionSelected && (
               <div className="suggestion-container">
                 <ul className="suggestion-list">
                   {endSuggestions.map((suggestion, index) => (
                     <li
                       key={index}
                       className="suggestion"
-                      onClick={() => handleSelectSuggestion(suggestion, 'endArticle', 'endUrl')}
+                      onClick={() => handleSelectSuggestion(suggestion, 'endArticle', 'endUrl', setEndSuggestionSelected)}
                     >
                       {suggestion}
                     </li>
@@ -112,8 +125,18 @@ const FormInput = ({ onSubmit }) => {
 
       {submittedData && (
         <div className="finding-route">
-          <h2>Finding Route:</h2>
-          <p>{submittedData.startArticle} To {submittedData.endArticle}</p>
+          {(formData.endArticle.length === 0 || formData.startArticle.length === 0) ? (
+            <p>Start and End cannot be blank!</p>
+          ) : (
+            (!startSuggestionSelected || !endSuggestionSelected) ? (
+              <p>Please select a suggestion for both start and end articles.</p>
+            ) : (
+              <div>
+                <h2>Finding Route:</h2>
+                <p>{submittedData.startArticle} To {submittedData.endArticle}</p>
+              </div>
+            )
+          )}
         </div>
       )}
     </div>
