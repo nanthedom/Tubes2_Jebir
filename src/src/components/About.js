757c9ac7d82fa94c211befe4344e9e3fb99f8ddc
NopalAul <mnaufal0605@gmail.com>
2024-04-18 14:46:53 +0700

    feat: style about us dikit

--- src/src/components/About.js
+++ src/src/components/About.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Header from './Header'
+import './About.css';
+import nopalImage from './assets/nopal.png';
+import gonzaImage from './assets/gonza.png';
+import adnanImage from './assets/adnan.png';
 
 function About() {
     return (
@@ -8,24 +12,30 @@ function About() {
                 <Header />
             </div>
             <div>
-                <h1 className="page-title">About Jebir Wikirace (contoh)</h1>
-                <p>
-                    Jebir Wikirace adalah website application yang memungkinkan pengguna untuk mencari rute terpendek antara dua artikel Wikipedia. Website ini dibuat oleh kelompok Jebir dengan anggota:
+                <h1 className="page-title">About Jebir Wikirace</h1>
+                <p className="content">
+                    Jebir Wikirace adalah website application yang memungkinkan pengguna untuk mencari rute terpendek antara dua artikel Wikipedia. Algoritma yang dapat digunakan adalah algoritma BFS dan IDS.
                 </p>
-                <ul>
-                    <li>
-                        <strong>Muhammad Naufal Aulia (13522074)</strong>
-                    </li>
-                    <li>
-                        <strong>Keanu Amadius Gonza Wrahatno (13522082)</strong>
-                    </li>
-                    <li>
-                        <strong>Naufal Adnan (13522116)</strong>
-                    </li>
-                </ul>
-                <p>
-                    Website ini merupakan realisasi atas Tugas Besar II Strategi Algoritma (IF2211)
+                <p className="content">
+                    Website ini merupakan realisasi atas Tugas Besar II Strategi Algoritma (IF2211). Dibuat oleh kelompok Jebir dengan anggota:
                 </p>
+                <div className="anggota-container">
+                    <div className="anggota-wrapper">
+                        <div className="nama-anggota">Muhammad Naufal Aulia</div>
+                        <img className="gambar-anggota" src={nopalImage} alt="inigambar"/>
+                        <div className="nim-anggota">13522074</div>
+                    </div>
+                    <div className="anggota-wrapper">
+                        <div className="nama-anggota">Keanu Amadius Gonza Wrahatno</div>
+                        <img className="gambar-anggota" src={gonzaImage} alt="inigambar"/>
+                        <div className="nim-anggota">13522082</div>
+                    </div>
+                    <div className="anggota-wrapper">
+                        <div className="nama-anggota">Naufal Adnan</div>
+                        <img className="gambar-anggota" src={adnanImage} alt="inigambar"/>
+                        <div className="nim-anggota">13522116</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
