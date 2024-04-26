f1d9ba59e14b1162de07b01c7186f7d4f8f71ca5
nanthedom <naufnan@gmail.com>
2024-04-27 00:49:27 +0700

    feat: enable all path (walaupun lama maaf yh)

--- src/backend/lib/back.go
+++ src/backend/lib/back.go
@@ -19,8 +19,8 @@ type Node struct {
 	Prev  *Node
 }
 
-func MainBackend(data FormData) ([]string, int, int, time.Duration, error) {
-	var paths []string
+func MainBackend(data FormData) ([][]string, int, int, time.Duration, error) {
+	var paths [][]string
 	var checkedArticle int
 	var clickArticle int
 	var excTime time.Duration
@@ -28,34 +28,43 @@ func MainBackend(data FormData) ([]string, int, int, time.Duration, error) {
 
 	if data.Algoritma == "BFS" {
 		fmt.Println("\nRute BFS")
-		// coba bonus
 		startTime := time.Now()
-		pathsBonus, checked, click, err := BFSBonus(data.StartUrl, data.EndUrl)
+		var path []string
+		path, checkedArticle, clickArticle, err = BFS(data.StartUrl, data.EndUrl)
+		paths = append(paths, path)
 		excTime = time.Since(startTime)
 		if err != nil {
 			return nil, 0, 0, 0, err
 		}
-		for _, path := range pathsBonus {
-			for _, url := range path {
-				fmt.Println(url)
-			}
-			fmt.Println()
-		}
-		fmt.Println(len(pathsBonus))
-		fmt.Println(checked)
-		fmt.Println(click)
-		paths, checkedArticle, clickArticle, err = BFS(data.StartUrl, data.EndUrl)
+
+	} else if data.Algoritma == "BFS All Path" {
+		fmt.Println("\nAll Path BFS")
+		startTime := time.Now()
+		paths, checkedArticle, clickArticle, err = BFSAllPath(data.StartUrl, data.EndUrl)
+		excTime = time.Since(startTime)
 		if err != nil {
 			return nil, 0, 0, 0, err
 		}
-	} else {
+
+	} else if data.Algoritma == "IDS" {
 		fmt.Println("\nRute IDS")
 		startTime := time.Now()
-		paths, checkedArticle, clickArticle, err = IDS(data.StartUrl, data.EndUrl)
+		var path []string
+		path, checkedArticle, clickArticle, err = IDS(data.StartUrl, data.EndUrl)
+		paths = append(paths, path)
+		excTime = time.Since(startTime)
 		if err != nil {
 			return nil, 0, 0, 0, err
 		}
+
+	} else {
+		fmt.Println("\nAll Path IDS")
+		startTime := time.Now()
+		paths, checkedArticle, clickArticle, err = IDSAllPath(data.StartUrl, data.EndUrl)
 		excTime = time.Since(startTime)
+		if err != nil {
+			return nil, 0, 0, 0, err
+		}
 	}
 
 	return paths, checkedArticle, clickArticle, excTime, nil
